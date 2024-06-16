cc02e36b9d20a30d0f1d367af99b3e8755f4b9b8
Ivo Stoykov <ivo.stoykov@abv.bg>
2024-06-16 23:26:54 +0000

    Changing the name of the answers in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,52 @@
 const quizQuestions = [{
         question: "Who promised that they would never lose another fight until they defeated a certain someone?",
         choices: ["Luffy", "Sanji", "Zoro", "Usopp"],
-        answer: 2
+        answer: "Zoro"
     },
     {
         question: "How did Luffy get the scar under his eye?",
         choices: ["Atacked by bandits", "Fight with animals", "Himself", "Fight with another villager"],
-        answer: 2
+         answer: "Himself"
     },
     {
         question: "Who was the first member of the SH crew to try and recruit a new member besides Luffy?",
         choices: ["Nami", "Sanji", "Zoro", "Usopp"],
-        answer: 0
+         answer: "Nami"
     },
     {
         question: "What made Crocodile join Luffy's 'Rescue Ace Crew'?",
         choices: ["Luffy", "Jinbei", "Ace", "Ivancov"],
-        answer: 3
+        answer: "Ace"
     },
     {
         question: "Who was the first marine Admiral to be shown in the series?",
         choices: ["Akainu", "Kizaru", "Aokiji", "Sengoku"],
-        answer: 2
+         answer: "Aokiji"
     },
     {
         question: "Who gave Shanks the scar on his eye?",
         choices: ["Dragon", "Marshall D", "Akainu", "Himself"],
-        answer: 1
+        answer: "Marshall D"
     },
     {
         question: "How many crewmates did Luffy say he wanted at the beginning of the series?",
         choices: ["9", "13", "10", "15"],
-        answer: 2
+         answer: "10"
     },
     {
         question: "Who said- Zoro should cut diamond next?",
         choices: ["Luffy", "Mihawk", "Kuina", "Daz Bones"],
-        answer: 3
+        answer: "Kuina"
     },
     {
         question: "Who was the first recruit of the SH crew?",
         choices: ["Nami", "Sanji", "Zoro", "Usopp"],
-        answer: 2
+        answer: "Zoro"
     },
     {
         question: "How many confirmed D clan members are there?",
         choices: ["8", "14", "11", "18"],
-        answer: 1
+         answer: "14"
     },
 ];
 
