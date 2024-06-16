d0e374cbfcf820f57683bfe394513ccc1cd68f0b
Ivo Stoykov <ivo.stoykov@abv.bg>
2024-06-16 23:44:45 +0000

    Calling all of the DOM elements by their ids in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Questions, options and asnwers array
 const quizQuestions = [{
         question: "Who promised that they would never lose another fight until they defeated a certain someone?",
         choices: ["Luffy", "Sanji", "Zoro", "Usopp"],
@@ -6,12 +7,12 @@ const quizQuestions = [{
     {
         question: "How did Luffy get the scar under his eye?",
         choices: ["Atacked by bandits", "Fight with animals", "Himself", "Fight with another villager"],
-         answer: "Himself"
+        answer: "Himself"
     },
     {
         question: "Who was the first member of the SH crew to try and recruit a new member besides Luffy?",
         choices: ["Nami", "Sanji", "Zoro", "Usopp"],
-         answer: "Nami"
+        answer: "Nami"
     },
     {
         question: "What made Crocodile join Luffy's 'Rescue Ace Crew'?",
@@ -21,7 +22,7 @@ const quizQuestions = [{
     {
         question: "Who was the first marine Admiral to be shown in the series?",
         choices: ["Akainu", "Kizaru", "Aokiji", "Sengoku"],
-         answer: "Aokiji"
+        answer: "Aokiji"
     },
     {
         question: "Who gave Shanks the scar on his eye?",
@@ -31,7 +32,7 @@ const quizQuestions = [{
     {
         question: "How many crewmates did Luffy say he wanted at the beginning of the series?",
         choices: ["9", "13", "10", "15"],
-         answer: "10"
+        answer: "10"
     },
     {
         question: "Who said- Zoro should cut diamond next?",
@@ -46,23 +47,39 @@ const quizQuestions = [{
     {
         question: "How many confirmed D clan members are there?",
         choices: ["8", "14", "11", "18"],
-         answer: "14"
+        answer: "14"
     },
 ];
-
-
+// Calling the Dom Elements by their ids
+let instructionsArea = document.getElementById('instructionsArea');
+let playButton = document.getElementById('playButton');
+let questionBox = document.getElementById('questionBox');
+let qestionContainer = document.getElementById('qestionContainer');
+let answersContainer = document.getElementById('answersContainer');
+let optionsElements = [
+    document.getElementById("answer0"),
+    document.getElementById("answer1"),
+    document.getElementById("answer2"),
+    document.getElementById("answer3")
+];
+let restartButton = document.getElementById('restartButton');
+let correct = document.getElementById('correct');
+let incorrect = document.getElementById('incorrect');
 
 function startGame() {
 
 }
 
+
 function restartGame() {
 
 }
 
+
 function displayScore() {
 
 }
+
 
 function showQuestion() {
 
